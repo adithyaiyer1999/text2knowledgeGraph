4964323334dc223b562602e4d49b597228dd1f52
srushti98 <srushtipawar12@gmail.com>
2023-12-09 04:02:53 -0500

    Added update, reset button. After updation the color of newly added node is changed to red.

--- text2Json_django/api/main_functions.py
+++ text2Json_django/api/main_functions.py
@@ -8,8 +8,9 @@ def createGraphFromText_(text):
     return str(response)
 
 def addToGraphFromText_(text, json_text):
-    query_prompt = "The user will provide a JSON string, which contains the current summary in a JSON tree format. The use will then provide a field Text To Be Added. You must understand the Text To Be Added, and update the JSON format provided with this new information. Return ONLY in JSON format. Nothing else." 
-    prompt = query_prompt + "\n\n Text To Be Added: " + text + "\n\nJSON: " + json_text + " \n\nUpdated JSON:"
+    query_prompt = "The user will provide a JSON string, which contains the current summary in a JSON tree format. The user will then provide a field Text To Be Added. You must understand the Text To Be Added, and update the JSON format provided with this new information. Your task is to take in the existing json text and append the new paragraph given into the form of json representation into the existing json. You cannot lose information of the old json. Return ONLY in JSON format. Nothing else."
+#     query_prompt="You will be given two inputs following this command, first is the a json string and second is a paragraph to update in the json string. The json tree is a knowledge tree which puts the information in a form of heirarchial structure, making the information text into a granular level json representation. Your task is to take in the existing json text and append the new paragraph given into the form of json representation into the existing json. You cannot lose information of the old json. Following are the paragraph and json."
+    prompt = query_prompt + "\n\n Paragraph : " + text + "\n\nJSON: " + json_text + " \n\nUpdated JSON:"
     response = openai_calls.ask_chatgpt(prompt)
     return str(response)
 
