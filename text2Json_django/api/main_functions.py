d6d806ba5c32f5916efa920f5c561ef592f96305
adithyaround <adithya@roundfinance.com>
2023-12-09 18:04:24 -0500

    added a new prompt for high detail graph generation. Added some additional cleaning after GPT-4 response so the model does not crash while parsing. Corrected 1 stupid bus where the UPDATE GRAPH prompt was being used for create prompt

--- text2Json_django/api/main_functions.py
+++ text2Json_django/api/main_functions.py
@@ -2,16 +2,27 @@ from . import openai_calls
 from . import prompts
 
 def createGraphFromText_(text):
-    model = "gpt-3.5-turbo"
-    query_prompt = prompts.CREATE_JSON_PROMPTS[model]
+    model = "gpt-4-1106-preview"
+    query_prompt = prompts.CREATE_HIGH_DETAIL_JSON_PROMPTS[model]
     prompt = query_prompt + "\n\n Text: " + text + "\n\nJSON:"
-    response = openai_calls.ask_chatgpt(prompt)
-    return str(response)
+    response = openai_calls.ask_chatgpt(prompt, model)
+
+    str_response = str(response)
+    
+    # Some sanity text cleaning to avoid errors in yaml loading
+    str_response = str_response.replace("json", "")
+    str_response = str_response.replace("`", "")
+    return str_response
 
 def addToGraphFromText_(text, json_text):
-    model = "gpt-3.5-turbo"
+    model = "gpt-4-1106-preview"
     query_prompt = prompts.UPDATE_JSON_PROMPTS[model]
     prompt = query_prompt + "\n\n Paragraph : " + text + "\n\nJSON: " + json_text + " \n\nUpdated JSON:"
-    response = openai_calls.ask_chatgpt(prompt)
-    return str(response)
+    response = openai_calls.ask_chatgpt(prompt, model)
+    str_response = str(response)
+    
+    # Some sanity text cleaning to avoid errors in yaml loading
+    str_response = str_response.replace("json", "")
+    str_response = str_response.replace("`", "")
+    return str_response
 
