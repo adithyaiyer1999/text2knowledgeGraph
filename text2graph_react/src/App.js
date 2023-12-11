5e58da0aee46e8bfe83b48605f098f827458060e
srushti98 <srushtipawar12@gmail.com>
2023-12-11 12:45:38 -0500

    Resolved the issue of pdf not maintaining the same session key

--- text2graph_react/src/App.js
+++ text2graph_react/src/App.js
@@ -231,6 +231,7 @@ function App() {
       formData.append('file', pdfFile);
       const response = await fetch('http://127.0.0.1:8000/api/create-graph-from-pdf/', {
           method: 'POST',
+          credentials: 'include',
           body: formData,
       });
     if (!response.ok) {
