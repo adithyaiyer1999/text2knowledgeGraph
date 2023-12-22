0baf39ac1a90900032f7d989bb138f41fc8a677b
adithyaround <adithya@roundfinance.com>
2023-12-22 17:52:52 -0500

    added eslint-disable-next-line no-unused-vars

--- text2graph_react/src/App.js
+++ text2graph_react/src/App.js
@@ -4,6 +4,7 @@ import './buffer_loading.css'
 
 function App() {
   const [input1, setInput1] = useState('');
+  // eslint-disable-next-line no-unused-vars
   const [input2, setInput2] = useState('');
   const [outputHtml, setOutputHtml] = useState('');
   const [isLoading, setIsLoading] = useState(false); // New loading state
