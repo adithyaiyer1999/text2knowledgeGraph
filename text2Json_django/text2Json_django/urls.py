46621e7288abc308f5d08c93738acb5d148c9de0
adithyaround <adithya@roundfinance.com>
2023-12-22 16:48:10 -0500

    increased timeout

--- text2Json_django/text2Json_django/urls.py
+++ text2Json_django/text2Json_django/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('/', admin.site.urls),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
